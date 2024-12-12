134bed0ba1a4680515861b64d08b54610297457d
IONHAX04 <thirukumara04@gmail.com>
2024-12-12 13:40:51 +0530

    Conditional render + Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+import PropTypes from "prop-types";
 
 import Header from "./components/00-Header/Header";
 import Dashboard from "./components/01-Dashboard/Dashboard";
@@ -12,29 +19,43 @@ import Messages from "./components/08-Messages/Messages";
 import Profile from "./components/09-Profile/Profile";
 import Settings from "./components/10-Settings/Settings";
 
+import Login from "./components/Login/Login";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Header>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/vendors" element={<Vendors />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/transactions" element={<Transactions />} />
-            <Route path="/reports" element={<Reports />} />
-            <Route path="/messages" element={<Messages />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
-        </Header>
-      </Router>
-    </>
+    <Router>
+      <ConditionalHeader>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/vendors" element={<Vendors />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/logout" element={<Login />} />
+        </Routes>
+      </ConditionalHeader>
+    </Router>
   );
 }
 
+function ConditionalHeader({ children }) {
+  const location = useLocation();
+
+  const excludedRoutes = ["/logout"];
+  const isExcluded = excludedRoutes.includes(location.pathname);
+
+  return isExcluded ? children : <Header>{children}</Header>;
+}
+
 export default App;
+
+ConditionalHeader.propTypes = {
+  children: PropTypes.node,
+};
